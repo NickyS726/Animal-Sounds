ffc7fb8af56eca95d520364ea1ab18c816011189
NickyS726 <119453601+NickyS726@users.noreply.github.com>
2023-01-06 13:06:07 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function cow() {
+    var audio = new Audio('mixkit-cow-moo-1744.wav');
+audio.play();
+}
+
+function dog() {
+    var audio = new Audio('mixkit-dog-barking-twice-1.wav');
+audio.play();
+}
+
+function cat() {
+    var audio = new Audio('mixkit-domestic-cat-hungry-meow-45.wav');
+audio.play();
+}
